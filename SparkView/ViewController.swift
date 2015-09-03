58d599b5bc704a0c37f6a26ab183d11e3ead4ac1
Josh <owner@owners-mbp.westell.com>
2015-09-02 22:47:50 -0400

    Initial Commit

---
+++ SparkView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SparkView
+//
+//  Created by Owner on 9/2/15.
+//  Copyright © 2015 Josh Coleman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
